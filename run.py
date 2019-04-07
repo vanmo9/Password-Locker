e7b4c371dbc085bd09d7c1d588a9ef2a1b9458d7
mohassan <vanmowha@gmail.com>
2019-04-07 12:59:12 +0300

    add short code in runpy finish

--- run.py
+++ run.py
@@ -110,3 +110,10 @@ def main():
                             break
                     else:
                             print("I really didn't get that. Please use the short codes")
+
+
+
+
+    if __name__ == '__main__':
+
+    main()
